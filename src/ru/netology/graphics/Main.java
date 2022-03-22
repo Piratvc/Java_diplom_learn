4e46b32e60aa5547b9223791a25498feab9dacb0
Piratvc <piratvc@gmail.com>
2022-03-22 15:25:50 +0300

    commit

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -6,10 +6,8 @@ import ru.netology.graphics.server.GServer;
 public class Main {
     public static void main(String[] args) throws Exception {
         TextGraphicsConverter converter = new Convert();
-
         GServer server = new GServer(converter);
         server.start();
-
 
     }
 }
